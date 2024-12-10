151fa01c4ac7628c864624fd255d71009b168f44
GITGARDS <adrianorogeriodesouza4353@gmail.com>
2024-12-10 20:55:55 -0300

    feat: first commit

---
+++ src/database/prisma-client.ts
@@ -0,0 +1,7 @@
+/*
+ *   Copyright (c) 2024 
+ *   All rights reserved.
+ */
+import { PrismaClient } from '@prisma/client';
+
+export const prisma = new PrismaClient();
---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,16 @@
+/*
+ *   Copyright (c) 2024 
+ *   All rights reserved.
+ */
+import { FastifyReply, FastifyRequest } from 'fastify';
+
+export async function authMiddleware(req: FastifyRequest, reply: FastifyReply) {
+  const apiEmail = req.headers['email'];
+
+  if (!apiEmail) {
+    reply.status(401).send({
+      message: 'Email is required',
+    });
+  }
+  
+}
